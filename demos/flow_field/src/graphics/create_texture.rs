6309070de08a4586ec71d4716a9ea5337c4ce636
Austin Theriot <austinmtheriot@gmail.com>
2022-07-16 14:45:15 -0500

    get perlin noise rendering into texture correctly

--- demos/flow_field/src/graphics/create_texture.rs
+++ demos/flow_field/src/graphics/create_texture.rs
@@ -1,4 +1,4 @@
-use js_sys::Math;
+use noise::{NoiseFn, Perlin};
 use wasm_bindgen::JsCast;
 use web_sys::{HtmlCanvasElement, WebGl2RenderingContext, WebGlTexture};
 use wrend::renderer::texture_create_context::TextureCreateContext;
@@ -39,21 +39,26 @@ pub fn create_texture<UserCtx>(ctx: TextureCreateContext<UserCtx>) -> WebGlTextu
     let canvas_width = canvas.width() as i32;
     let canvas_height = canvas.height() as i32;
 
+    let perlin = Perlin::new();
+
     // generate a texture of noise
     let bytes_per_pixel = 4;
     let length_of_noise_array = (canvas_width * canvas_height * bytes_per_pixel) as usize;
     let mut noise_image = Vec::with_capacity(length_of_noise_array);
-    for _ in 0..length_of_noise_array {
-        noise_image.push(0u8);
-    }
-    for rgba in noise_image.chunks_mut(bytes_per_pixel as usize) {
-        let random_float = Math::random();
-        let black_or_white = if random_float < 0.5 { u8::MIN } else { u8::MAX };
-
-        rgba[0] = black_or_white;
-        rgba[1] = black_or_white;
-        rgba[2] = black_or_white;
-        rgba[3] = u8::MAX;
+    const SECTIONS: f64 = 5.0;
+    for x in 0..canvas_width {
+        for y in 0..canvas_height {
+            // input is expected to be between (0 and 1 ?)
+            let x = x as f64 / (canvas_width as f64 / SECTIONS);
+            let y = y as f64 / (canvas_height as f64 / SECTIONS);
+            let perlin_noise = perlin.get([x, y]);
+            // map to (0, 1)
+            let perlin_noise = perlin_noise * 0.5 + 0.5;
+            // map to (0, 255)
+            let perlin_noise = perlin_noise * (u8::MAX as f64);
+            let perlin_noise = perlin_noise as u8;
+            noise_image.extend([perlin_noise, perlin_noise, perlin_noise, u8::MAX]);
+        }
     }
 
     gl.tex_image_2d_with_i32_and_i32_and_i32_and_format_and_type_and_u8_array_and_src_offset(
