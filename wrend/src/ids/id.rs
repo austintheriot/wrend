2b45b7c67bceb4d483fe05ddda6ee2e373ae6b67
Austin Theriot <austinmtheriot@gmail.com>
2022-07-17 20:42:12 -0500

    implement Id and IdName for string and some numbers

--- wrend/src/ids/id.rs
+++ wrend/src/ids/id.rs
@@ -3,3 +3,27 @@ use std::hash::Hash;
 
 /// Collection of traits that make something useful as an id for programs, shaders, uniforms, etc.
 pub trait Id: 'static + Hash + PartialEq + Eq + Clone + Debug + Default {}
+
+impl Id for String {}
+
+impl Id for &'static str {}
+
+impl Id for u8 {}
+
+impl Id for u16 {}
+
+impl Id for u32 {}
+
+impl Id for u64 {}
+
+impl Id for u128 {}
+
+impl Id for i8 {}
+
+impl Id for i16 {}
+
+impl Id for i32 {}
+
+impl Id for i64 {}
+
+impl Id for i128 {}
--- wrend/src/ids/id_name.rs
+++ wrend/src/ids/id_name.rs
@@ -2,3 +2,15 @@
 pub trait IdName {
     fn name(&self) -> String;
 }
+
+impl IdName for String {
+    fn name(&self) -> String {
+        self.to_owned()
+    }
+}
+
+impl IdName for &'static str {
+    fn name(&self) -> String {
+        self.to_string()
+    }
+}
