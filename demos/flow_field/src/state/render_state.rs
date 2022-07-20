1507e6bb125b0c81f00a614de80bacef9fdf7e15
Austin Theriot <austinmtheriot@gmail.com>
2022-07-19 22:36:42 -0500

    increase particles to 200,000

--- demos/flow_field/src/state/render_state.rs
+++ demos/flow_field/src/state/render_state.rs
@@ -13,7 +13,7 @@ pub struct RenderState {
 impl Default for RenderState {
     fn default() -> Self {
         Self {
-            num_particles: 100_000,
+            num_particles: 200_000,
             count: 0,
         }
     }
