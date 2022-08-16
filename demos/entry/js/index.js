e1ecaa57af3f4906e161513a1f083cef40030bd9
Austin Theriot <austinmtheriot@gmail.com>
2022-08-15 23:50:59 -0500

    add sass support

--- demos/entry/js/index.js
+++ demos/entry/js/index.js
@@ -1,1 +1,3 @@
+import '../css/style.scss';
+
 import("../pkg/index.js").catch(console.error).then((module) => module.entry());
--- demos/entry/webpack.config.js
+++ demos/entry/webpack.config.js
@@ -41,6 +41,18 @@ module.exports = (env, argv) => {
       path: distPath,
       filename: "main.js",
     },
+    module: {
+      rules: [
+        {
+          test: /\.s[ac]ss$/i,
+          use: [
+            'style-loader',
+            'css-loader',
+            'sass-loader',
+          ],
+        },
+      ],
+    },
     plugins: [
       new CopyPlugin({
         patterns: [{
