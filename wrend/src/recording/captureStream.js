e8cb65966eb0f61e4cfe53686e502afc8e9c91ef
Austin Theriot <austinmtheriot@gmail.com>
2022-08-18 00:30:46 -0500

    fix typo

--- wrend/src/recording/captureStream.js
+++ wrend/src/recording/captureStream.js
@@ -1,6 +1,6 @@
 // web_sys does not provide this functionality out of the box 
 // (perhaps because of low-ish browser support?)
-// so mut implement it manually
+// so it must be implemented manually here in JavaScript
 export function captureStreamFromCanvas(canvas) {
     const mediaStream = canvas.captureStream();
     console.log({mediaStream});
