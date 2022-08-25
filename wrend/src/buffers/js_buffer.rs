aa3ca8c944cd84e9f2789f3f0307200a8502eed4
Austin Theriot <austinmtheriot@gmail.com>
2022-08-24 23:45:31 -0500

    implement methods for JsBuffer, remove js_callback_with_context

--- wrend/src/buffers/js_buffer.rs
+++ wrend/src/buffers/js_buffer.rs
@@ -1,10 +1,37 @@
 use crate::Buffer;
+use std::ops::{Deref, DerefMut};
 use wasm_bindgen::prelude::wasm_bindgen;
+use web_sys::WebGlBuffer;
 
 pub type JsBufferInner = Buffer<String>;
 
 #[wasm_bindgen]
 pub struct JsBuffer(JsBufferInner);
+
+#[wasm_bindgen]
+impl JsBuffer {
+    pub fn buffer_id(&self) -> String {
+        self.deref().buffer_id().to_owned()
+    }
+
+    pub fn webgl_buffer(&self) -> WebGlBuffer {
+        self.deref().webgl_buffer().to_owned()
+    }
+}
+
+impl Deref for JsBuffer {
+    type Target = JsBufferInner;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+impl DerefMut for JsBuffer {
+    fn deref_mut(&mut self) -> &mut JsBufferInner {
+        &mut self.0
+    }
+}
 
 impl From<JsBufferInner> for JsBuffer {
     fn from(js_buffer_inner: JsBufferInner) -> Self {
--- wrend/src/callbacks.rs
+++ wrend/src/callbacks.rs
@@ -1,5 +1,3 @@
 mod callback_with_context;
-mod js_callback_with_context;
 
 pub use callback_with_context::*;
-pub use js_callback_with_context::*;
