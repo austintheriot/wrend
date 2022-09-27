95e49691d69b8706f3758b5004d3f68284bb1e64
Austin Theriot <austinmtheriot@gmail.com>
2022-09-26 20:46:50 -0500

    take some screenshots

--- demos/kaleidoscope/src/shaders/filter_split.glsl
+++ demos/kaleidoscope/src/shaders/filter_split.glsl
@@ -8,7 +8,9 @@ uniform sampler2D u_src_texture;
 
 out vec4 out_color;
 
+const float UNEVEN_TRANSPOSITION = 23.0 / 97.0;
+
 void main() {
-  vec2 split_coords = mod(v_tex_coord + 0.25, 1.0);
+  vec2 split_coords = mod(v_tex_coord + UNEVEN_TRANSPOSITION, 1.0);
   out_color = texture(u_src_texture, split_coords);
 }
