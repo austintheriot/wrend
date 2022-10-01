4d578ceed0bb4e4f316f3aa79ee88a99b9aa8f80
Austin Theriot <austinmtheriot@gmail.com>
2022-10-01 15:21:01 -0500

    adjust larger_than_life styles

--- demos/larger_than_life/src/components/app.rs
+++ demos/larger_than_life/src/components/app.rs
@@ -130,7 +130,7 @@ pub fn app() -> Html {
     );
 
     html! {
-        <div class="larger-than-life">
+        <div>
             <Link<Route> to={Route::Home}>{"Home"}</Link<Route>>
             <canvas ref={canvas_ref} height={1000} width={1000} />
         </div>
