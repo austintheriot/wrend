b813271305ab2c722dbae1a0714177af5c3b0207
Austin Theriot <austinmtheriot@gmail.com>
2022-10-01 23:10:54 -0500

    add correct images for each preview thumbnail

--- demos/entry/src/pages/home.rs
+++ demos/entry/src/pages/home.rs
@@ -1,6 +1,5 @@
 use shared::route::Route;
 use yew::prelude::*;
-use yew_router::prelude::*;
 use crate::components::LinkCard;
 
 #[function_component(Home)]
@@ -8,13 +7,14 @@ pub fn home() -> Html {
     html! {
     <div class="home">
         <h1>
-           {"Demos"}
+           {"Wrend Live Demos"}
         </h1>
         <div class="link-card-container">
             <LinkCard 
                 route={Route::HelloQuad}
                 title="Hello Quad"
                 img_src="./assets/hello_quad.png"
+                img_alt="Multicolored quadrilateral"
             />
             <LinkCard 
                 route={Route::HelloQuadAnimated}
@@ -24,32 +24,37 @@ pub fn home() -> Html {
             <LinkCard 
                 route={Route::RecordingDemo}
                 title="Recording Demo"
-                img_src="./assets/hello_quad.png"
+                vid_src="./assets/hello_quad_animated.webm"
             />
             <LinkCard 
                 route={Route::GameOfLife}
                 title="Game of Life"
-                img_src="./assets/hello_quad.png"
+                img_src="./assets/game_of_life.png"
+                img_alt="Conway's classic game of life"
             />
             <LinkCard 
                 route={Route::LargerThanLife}
                 title="Larger Than Life"
-                img_src="./assets/hello_quad.png"
+                img_src="./assets/larger_than_life.png"
+                img_alt="Render of the Game of Life, adjusted to a larger kernel size"
             />
             <LinkCard 
                 route={Route::SimplexNoise}
                 title="Simplex Noise"
-                img_src="./assets/hello_quad.png"
+                img_src="./assets/simplex_noise.png"
+                img_alt="Basic 2d noise render using simplex noise"
             />
             <LinkCard 
                 route={Route::FlowField}
                 title="Flow Field"
-                img_src="./assets/hello_quad.png"
+                img_src="./assets/flow_field.png"
+                img_alt="Particles flowing across a 2D plane"
             />
             <LinkCard 
                 route={Route::RayTracer}
                 title="Ray Tracer"
-                img_src="./assets/hello_quad.png"
+                img_src="./assets/ray_tracer.png"
+                img_alt="Render from a custom ray tracer"
             />
        </div>
     </div>
